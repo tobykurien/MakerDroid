004131891ac8598fa33321cd726affdd6a82e842
skullkey <schalk.heunis@gmail.com>
2012-02-14 18:12:57 +0200

    fixed filename in generateGcode

--- src/za/co/house4hack/paint3d/gcode/PythonRunner.java
+++ src/za/co/house4hack/paint3d/gcode/PythonRunner.java
@@ -3,13 +3,17 @@ package za.co.house4hack.paint3d.gcode;
 import java.util.List;
 import java.util.Map;
 
+import android.util.Log;
+
 public class PythonRunner {
 
+	private static final String TAG = "PythonRunner";
 	public static void executePythonCode(List<String> libs, Map<String,String> env, String code){
 		
 		
 		  for(String l:libs) System.loadLibrary(l);
 		  for(String k:env.keySet()) nativeSetEnv(k, env.get(k));
+		  Log.d(TAG,code);
 		  runPython(code);
 
 	}
